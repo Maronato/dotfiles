0acbb11463bddd967a2fcb4c5a1e977f23bc2660
Gustavo Maronato <gustavomaronato@gmail.com>
2019-01-07 18:20:23 -0200

    add install script for zsh and oh-my-zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -13,23 +13,15 @@ then
   if test "$(uname)" = "Darwin"
   then
     sudo sh -c 'echo /usr/local/bin/zsh >> /etc/shells'
-  fi
-
-fi
-
-# Check if zsh is the default shell
-if test $(which zsh) != $SHELL
-then
-  echo "  Setting ZSH as default"
-  # Install the correct homebrew for each OS type
-  if test "$(uname)" = "Darwin"
-  then
+    echo "  Setting ZSH as default"
     chsh -s $(which zsh)
   fi
 
+  if [ ! -d "$ZSH" ]
+  then
+    echo "  Installing oh-my-zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  fi
 fi
-
-# Install oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 exit 0
